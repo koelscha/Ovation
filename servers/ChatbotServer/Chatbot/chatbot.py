1bea4e512460b59a6bfed919ab11d4ceed77a9a1
Domink Bermühler <dominik.bermuehler@googlemail.com>
2017-09-20 19:37:58 +0200

    Implemented the Chatbot logic (not working yet).

--- servers/ChatbotServer/Chatbot/chatbot.py
+++ servers/ChatbotServer/Chatbot/chatbot.py
@@ -3,7 +3,11 @@ from flask_restful import Resource, Api
 import requests
 import urllib.parse as urlparse
 import json
+from enum import Enum
 
+import IntentClassifier
+from BusinessCase import BusinessCase
+from Entity import Entity
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,7 +16,7 @@ api = Api(app)
 
 serverAddress = "192.168.54.37:8080"
 #serverAddress = "127.0.0.1:8080"
-chatBotAddress = "192.168.54.26:5000"
+chatBotAddress = "192.168.54.32:5000"
 
 http = requests.Session()
 headers = {'content-type': 'application/json'}
@@ -29,31 +33,70 @@ def register():
         print('Request failed with http status ' + str(r.status_code))
 
 
+class State(Enum):
+    init = 1
+    extract = 2
+
 
 class ChatBot(Resource):
+    __name__ = "ChatBot"
+    def __init__(self):
+        self.state = State.init
+        entities = [Entity("What is your name?", lambda message: message)]
+        #self.businessCases = {"greeting": BusinessCase("greeting", confirmationPhrase="How can help you?"),
+        #                      "contract": BusinessCase("contract", entities, confirmationPhrase="Your name is {}.")}
+        self.businessCases = [BusinessCase("greeting", confirmationPhrase="How can help you?"),
+                              BusinessCase("contract", entities, confirmationPhrase="Your name is {}.")]
+        self.currentBusinessCase = None
+        self.currentEntity = None
+
+
     def onMessageReceived(self, message, clientId):
-        print(message)
-        print(clientId)
-        data = {'clientId': '1', 'message': 'test'}
+        if self.state is State.init:
+            intent = IntentClassifier.classify(message)
 
-        r = http.post(serverMessage, data=json.dumps(data), headers=headers)
-        try:
-            r.raise_for_status()
-        except requests.exceptions.HTTPError:
-            print('Request failed with http status ' + str(r.status_code))
+            #self.currentBusinessCase = self.businessCases[intent]
+            #self.state = State.extract
+            for businessCase in self.businessCases:
+                if intent == businessCase.intent:
+                    self.currentBusinessCase = businessCase
+                    self.state = State.extract
+                    break
 
+            self.currentEntity = self.currentBusinessCase.getNextEmptyEntity()
+            if self.currentEntity:
+                self.sendMessage(clientId, self.currentEntity.question)
+                self.state = State.extract
+        elif self.state is State.extract:
+            self.currentEntity.value = self.currentEntity.extract(message)
+            self.currentEntity = self.currentBusinessCase.getNextEmptyEntity()
+
+        if not self.currentEntity:
+            self.sendMessage(clientId, self.currentBusinessCase.confirmationPhrase)
+            self.currentBusinessCase = None
+
+
+    def sendMessage(selfs, clientId, message):
+        data = {'clientId': clientId, 'message': message}
+        print("Message sent: " + json.dumps(data))
+        #r = http.post(serverMessage, data=json.dumps(data), headers=headers)
+        #try:
+        #    r.raise_for_status()
+        #except requests.exceptions.HTTPError:
+        #    print('Request failed with http status ' + str(r.status_code))
 
     def post(self):
         message, clientId = request.get_json()["message"], request.get_json()["clientId"]
         self.onMessageReceived(message, clientId)
 
+chatbot = ChatBot()
 
-api.add_resource(ChatBot, '/message')
+api.add_resource(chatbot, '/message')
 
 
 if __name__ == '__main__':
     try:
-        register()
+        #register()
         app.run(host='0.0.0.0')
     except:
         pass
