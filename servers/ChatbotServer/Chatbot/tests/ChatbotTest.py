d79673545e50b43c3ec8b6a77c33bae1712c8021
Dominik Bermühler <dominik.bermuehler@googlemail.com>
2017-09-21 12:34:25 +0200

    Refactored chatbot test.

--- servers/ChatbotServer/Chatbot/tests/ChatbotTest.py
+++ servers/ChatbotServer/Chatbot/tests/ChatbotTest.py
@@ -7,16 +7,14 @@ class ChatbotTest(unittest.TestCase):
     def setUp(self):
         self.chatbot = ChatBot()
 
-    def test_greeting(self):
-        self.assertEqual(self.chatbot.processMessage("Hello Ovation", "1"), "How can help you?")
-
-    def test_contract(self):
-        self.assertEqual(self.chatbot.processMessage("I want to move!", "1"), "What is your name?")
-
     def test_answer_with_name(self):
         self.chatbot.processMessage("I want to move!", "1")
         self.assertEqual(self.chatbot.processMessage("Max Mustermann", "1"), "Your name is {}.")
 
+    def test_greeting_with_contract(self):
+        self.assertEqual(self.chatbot.processMessage("Hello Ovation", "1"), "How can help you?")
+        self.chatbot.processMessage("I want to move!", "1")
+        self.assertEqual(self.chatbot.processMessage("Max Mustermann", "1"), "Your name is {}.")
 
 if __name__ == '__main__':
     unittest.main()
