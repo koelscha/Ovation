a5cbe97484e87b31670c0bbd82fa75d71e0b85fc
Dominik Bermühler <dominik.bermuehler@googlemail.com>
2017-09-21 09:46:11 +0200

    Refactored ChatbotServer.py

--- servers/ChatbotServer/Chatbot/ChatbotServer.py
+++ servers/ChatbotServer/Chatbot/ChatbotServer.py
@@ -6,35 +6,46 @@ import json
 
 from chatbot import ChatBot
 
-app = Flask(__name__)
-api = Api(app)
 
-serverAddress = "192.168.54.37:8080"
-#serverAddress = "127.0.0.1:8080"
-chatBotAddress = "192.168.54.32:5000"
+class ChatbotServer:
+    chatbot = ChatBot()
 
-http = requests.Session()
-headers = {'content-type': 'application/json'}
-myEndpoint = {'url': urlparse.urlunparse(('http', chatBotAddress, '/message', '', '', ''))}
-serverRegister = urlparse.urlunparse(('http', serverAddress, '/chatbot', '', '', ''))
-serverMessage = urlparse.urlunparse(('http', serverAddress, '/message', '', '', ''))
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.api = Api(self.app)
+        self.api.add_resource(MessageHandler, '/message')
 
-chatbot = ChatBot()
+        self.serverAddress = "192.168.54.37:8080"
+        #self.serverAddress = "127.0.0.1:8080"
+        self.chatBotAddress = "192.168.54.32:5000"
 
-def register():
-    r = http.post(serverRegister, data=json.dumps(myEndpoint), headers=headers)
-    try:
-        r.raise_for_status()
-    except requests.exceptions.HTTPError:
-        print('Request failed with http status ' + str(r.status_code))
+        self.myMessageURL = {'url': urlparse.urlunparse(('http', self.chatBotAddress, '/message', '', '', ''))}
+        self.smoopeRegisterURL = urlparse.urlunparse(('http', self.serverAddress, '/chatbot', '', '', ''))
+        self.smoopeMessageURL = urlparse.urlunparse(('http', self.serverAddress, '/message', '', '', ''))
 
-class ChatbotServer(Resource):
+
+    def register(self):
+        http = requests.Session()
+        headers = {'content-type': 'application/json'}
+        response = http.post(self.smoopeRegisterURL, data=json.dumps(self.myMessageURL), headers=headers)
+        try:
+            response.raise_for_status()
+        except requests.exceptions.HTTPError:
+            print('Request failed with http status ' + str(response.status_code))
+
+
+    def start(self):
+        #self.register()
+        self.app.run(host='0.0.0.0')
+
+
+class MessageHandler(Resource):
     def post(self):
         message, clientId = request.get_json()["message"], request.get_json()["clientId"]
         self.onMessageReceived(message, clientId)
 
     def onMessageReceived(self, message, clientId):
-        answer = chatbot.processMessage(message, clientId)
+        answer = ChatbotServer.chatbot.processMessage(message, clientId)
         self.sendMessage(clientId, answer)
 
     def sendMessage(selfs, clientId, message):
@@ -46,12 +57,6 @@ class ChatbotServer(Resource):
         # except requests.exceptions.HTTPError:
         #    print('Request failed with http status ' + str(r.status_code))
 
-api.add_resource(ChatbotServer, '/message')
-
 
 if __name__ == '__main__':
-    try:
-        #register()
-        app.run(host='0.0.0.0')
-    except:
-        pass
+    ChatbotServer().start()
