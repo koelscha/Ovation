5b0d7bd1434f5fa0547dfc37f54e0086420b9a2c
Dominik Bermühler <dominik.bermuehler@googlemail.com>
2017-09-21 23:08:41 +0200

    Added test cases to recognize "contract" test cases.

--- servers/ChatbotServer/Chatbot/tests/IntentClassifierTests.py
+++ servers/ChatbotServer/Chatbot/tests/IntentClassifierTests.py
@@ -1,4 +1,3 @@
-import os
 import unittest
 
 from IntentClassifiers.RasaClassifier import RasaClassifier
@@ -6,22 +5,23 @@ from IntentClassifiers.RasaClassifier import RasaClassifier
 
 class IntentClassifierTests(unittest.TestCase):
     def setUp(self):
-        print(os.getcwd())
         rasa_model = '../rasa/models/model_20170921-221417'
         rasa_config = "../rasa/config_spacy.json"
         self.rasa_classifier = RasaClassifier(rasa_model, rasa_config)
 
     def test_greeting_recognition(self):
-        test_messages = ["Hi there.",
-                         "Hey yo whats up?",
-                         "Hello my name is Max Mustermann."]
+        test_messages = {"greeting": ["Hi there.",
+                                      "Hey yo whats up?",
+                                      "Hello my name is Max Mustermann."],
+                         "contract": ["I want to move.",
+                                      "Can you change my address?",
+                                      "I want to change my address."]
+                         }
 
-        for message in test_messages:
-            self.assertEqual(self.rasa_classifier.classify(message), "greeting",
-                             "The intent of \"{}\" was not correct!".format(message))
-
-    def test_contract_recognition(self):
-        pass
+        for intent, messages in test_messages.items():
+            for message in messages:
+                self.assertEqual(self.rasa_classifier.classify(message), intent,
+                                 "The intent of \"{}\" was not correct!".format(message))
 
 
 if __name__ == '__main__':
