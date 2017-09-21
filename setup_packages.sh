b3a25d697e8b65a0e504e7324b599d6394b8e694
koelscha <koelscha@infokl.duckdns.org>
2017-09-22 00:48:28 +0200

    Fixed RASA

--- setup_packages.sh
+++ setup_packages.sh
@@ -1,10 +1,6 @@
 # Install the bleeding edge version of TFLearn and rasa
 pip install git+https://github.com/tflearn/tflearn.git
-
-git clone https://github.com/RasaHQ/rasa_nlu.git /tmp/rasa_nlu
-cd !$
-pip install -r requirements.txt
-python setup.py install
+pip install git+https://github.com/RasaHQ/rasa_nlu.git
 
 # Download the data and Models of NLTK and Spacy
 python -m nltk.downloader all
