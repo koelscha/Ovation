32fcb1c41494fdf16b09a71b16b9ab6f30501ec4
Marc Dahlem <M.Dahlem@insiders-technologies.de>
2017-09-22 13:16:05 +0200

    Entity extractor is read from json

--- servers/ChatbotServer/Chatbot/Entity.py
+++ servers/ChatbotServer/Chatbot/Entity.py
@@ -1,6 +1,13 @@
+import importlib
+
 class Entity:
     def __init__(self, config):
         self.name = config["name"]
         self.question = config["question"]
+        if "extractor" in config:
+            module = importlib.import_module("EntityExtractors." + config["extractor"])
+            self.extractor = getattr(module, config["extractor"])()
+        else:
+            self.extractor = None
         self.value = None
         self.confidence = None
