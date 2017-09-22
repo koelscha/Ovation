4425654f082cc1dd917bbc18727da2ed55c93388
koelscha <koelscha@infokl.duckdns.org>
2017-09-22 05:46:29 +0200

    added streetnumber to bot answer, insurance case

--- servers/ChatbotServer/Chatbot/BusinessLogic/InsuranceCalculator.py
+++ servers/ChatbotServer/Chatbot/BusinessLogic/InsuranceCalculator.py
@@ -2,12 +2,13 @@ from BusinessLogic import BusinessLogic
 
 class InsuranceCalculator(BusinessLogic):
     def processEntities(self, entities):
-        return ("We updated your address to {}, {} {}\n"
+        return ("We updated your address to {} {}, {} {}\n"
         "Based on {} square meters the annual fee for your household insurance will be 56€ (previously 52€) "
         "starting from {}.").format(self.getValueByName(entities, 'street'),
-                                  self.getValueByName(entities, 'zip'),
-                                  self.getValueByName(entities, 'city'),
-                                  self.getValueByName(entities, 'area'),
-                                  self.getValueByName(entities, 'date'))
+                                    self.getValueByName(entities, 'streetnumber'),
+                                    self.getValueByName(entities, 'zip'),
+                                    self.getValueByName(entities, 'city'),
+                                    self.getValueByName(entities, 'area'),
+                                    self.getValueByName(entities, 'date'))
 
 
