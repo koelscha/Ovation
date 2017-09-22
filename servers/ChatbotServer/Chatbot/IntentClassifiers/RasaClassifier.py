6e58e7d7185d86a234671f72588752f57bccf96b
Dominik Bermühler <dominik.bermuehler@googlemail.com>
2017-09-22 12:00:15 +0200

    Trained rasa intent classifier on new data and added bicycle business cases.

--- servers/ChatbotServer/Chatbot/IntentClassifiers/RasaClassifier.py
+++ servers/ChatbotServer/Chatbot/IntentClassifiers/RasaClassifier.py
@@ -1,6 +1,3 @@
-
-from IntentClassifiers import IntentClassifier
-
 from rasa_nlu.config import RasaNLUConfig
 from rasa_nlu.model import Interpreter
 
@@ -9,7 +6,7 @@ from IntentClassifiers import IntentClassifier
 
 class RasaClassifier(IntentClassifier):
     def __init__(self, rasa_model_dir=None, rasa_config_file=None):
-        rasa_model_dir = 'rasa/models/default/model_20170922-031359'
+        rasa_model_dir = 'rasa/models/default/model_20170922-115414'
         rasa_config_file = "rasa/config_spacy.json"
         self.interpreter = Interpreter.load(rasa_model_dir, RasaNLUConfig(rasa_config_file))
 
