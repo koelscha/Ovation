9d6b7f8800892d66c4a94d9b2050c8168c046740
Dominik Bermühler <dominik.bermuehler@googlemail.com>
2017-09-22 14:12:31 +0200

    Added bike insurance to presentation test.

--- servers/ChatbotServer/Chatbot/tests/PresentationTest.py
+++ servers/ChatbotServer/Chatbot/tests/PresentationTest.py
@@ -8,38 +8,13 @@ class PresentationTest(unittest.TestCase):
         fileName = "../../json-schemas/examples/presentation.json"
         self.chatbot = ChatBot(fileName)
 
-    def test_answer_with_name(self):
-        self.assertEqual(self.chatbot.processMessage("Hello", "1"), "Hello, what can I do for you?")
-        self.assertTrue(
-            self.chatbot.processMessage("I am going to move soon, and I am having trouble about the price change",
-                                        "1").find("When you move we need your new address") >= 0)
-        self.assertEqual(self.chatbot.processMessage("01.10.2017", "1"), "What is the new street name and number?")
-        self.assertEqual(self.chatbot.processMessage("Saarbrückerstraße 36", "1"), "What is the new zip code?")
-        self.assertEqual(self.chatbot.processMessage("10787", "1"), "How large is the new home in square meters?")
-        self.assertEqual(self.chatbot.processMessage("104", "1"), "When will you move into your new home?")
-        result = self.chatbot.processMessage("1st of October", "1")
-        self.assertTrue(result.find("Saarbrückerstraße 36")>=0)
-        self.assertTrue(result.find("10787")>=0)
-        self.assertTrue(result.find("Berlin")>=0)
-        self.assertTrue(result.find("1st of October")>=0)
-        self.assertTrue(result.find("104") >= 0)
-
-    def testLostInsuranceCard(self):
-        self.assertEqual(self.chatbot.processMessage("Hello", "1"), "Hello, what can I do for you?")
-        self.assertTrue(self.chatbot.processMessage("I have lost my insurance card. What should I do?", "1").find(
-            "No problem") >= 0)
-        self.assertEqual(self.chatbot.processMessage("Oh, great", "1"), "What is your insurance number?")
-        self.assertEqual(self.chatbot.processMessage("1234", "1"), "What is your birthdate?")
-        self.assertTrue(self.chatbot.processMessage("01.02.2001", "1").find("1234") >= 0)
-
-    def test_entity_recognizer(self):
-        fileName = "../../json-schemas/examples/entity_recognizer_test.json"
+    def test_bike(self):
+        fileName = "../../json-schemas/examples/presentation.json"
         self.chatbot = ChatBot(fileName)
-        self.assertEqual(self.chatbot.processMessage("Hello", "1"), "Hello, what can I do for you?")
-        self.assertTrue(self.chatbot.processMessage("Contract", "1").find("When you move we need your new address") >= 0)
-        self.assertEqual(self.chatbot.processMessage(
-            "My new flat is located in Saarbrücker Straße 36, 10787 Berlin and has 64 square meters.", "1"),
-                         "When will you move into your new home?")
+        self.assertEqual(self.chatbot.processMessage("bike", "1"),
+                         "I will check that for you. Can you tell me the cost of your bycicle or send a photo of the invoice")
+        self.assertEqual(self.chatbot.processMessage("Ok.", "1"), "What was the price of your bicycle?")
+        self.assertEqual(self.chatbot.processMessage("500", "1"), "Bikes are included up to a value of 500 €")
 
 if __name__ == '__main__':
     unittest.main()
