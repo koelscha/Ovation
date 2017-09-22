2334b906807307fbc54fe7cd7a6124f6a780f433
koelscha <koelscha@infokl.duckdns.org>
2017-09-22 05:22:27 +0200

    Changed rasa model path

--- servers/ChatbotServer/Chatbot/IntentClassifiers/RasaClassifier.py
+++ servers/ChatbotServer/Chatbot/IntentClassifiers/RasaClassifier.py
@@ -9,8 +9,8 @@ from IntentClassifiers import IntentClassifier
 
 class RasaClassifier(IntentClassifier):
     def __init__(self, rasa_model_dir=None, rasa_config_file=None):
-        rasa_model_dir = '../rasa/models/default/model_20170922-031359'
-        rasa_config_file = "../rasa/config_spacy.json"
+        rasa_model_dir = 'rasa/models/default/model_20170922-031359'
+        rasa_config_file = "rasa/config_spacy.json"
         self.interpreter = Interpreter.load(rasa_model_dir, RasaNLUConfig(rasa_config_file))
 
     def classify(self, message):
